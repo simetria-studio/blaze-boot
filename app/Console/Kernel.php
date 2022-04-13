7a9e87689acad4a7eed2e7266a2c5c8d7113c576
Felipe Cruz <felipephplow@gmail.com>
2022-04-13 17:37:46 -0300

    a

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->exec('node ../leilosoc-webscraping')->everyMinute();
+        $schedule->exec('node ../leilosoc-webscraping')->cron('*/2 * * * *');
     }
 
     /**
